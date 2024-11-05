d0e7bbb85631ee62f596d8ccb8be1bf5b0da2822
Reshad Haque <reshadulhaque1@gmail.com>
2024-11-05 11:22:54 -0800

    Initial commit

---
+++ d_ff_enable.sv
@@ -0,0 +1,12 @@
+module d_ff_enable(
+    input logic d, clk, reset, en,
+    output logic q
+    );
+    always_ff@(posedge clk, posedge reset)
+    begin
+        if(reset)
+            q<=0;
+        else if(en)
+            q<=d;
+    end
+endmodule
